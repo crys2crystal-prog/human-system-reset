af7c09242d1c3491ae9c80cd6f3e3607b816f065
crys2crystal-prog <crys2crystal@gmail.com>
2026-09-03 15:30:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 app.use(express.static(__dirname + "/public"));
+app.get("/presenter.html", (req, res) => {
+  res.sendFile(__dirname + "/public/presenter.html");
+});
 
 const PORT = process.env.PORT || 3000;
 
